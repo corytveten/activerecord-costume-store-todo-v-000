0bbc7aec92e37bfae9669117d570f5a2477598b2
Cory Tveten <corytveten@gmail.com>
2018-08-20 20:02:32 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.integer :price
       t.string :size
       t.string :image_url
-      t.datetime
+      t.timestamps
     end
   end
 end
